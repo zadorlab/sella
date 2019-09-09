59741b8d4534d2e253adaa2b9650cb3aae8b7ba2
Eric Hermes <ehermes@sandia.gov>
2019-09-09 16:44:18 -0700

    Replace irc function with IRC class inheriting from ASE Optimizer

--- sella/irc.py
+++ sella/irc.py
@@ -7,9 +7,13 @@ import warnings
 import numpy as np
 from scipy.linalg import eigh
 
+from sella.peswrapper import PESWrapper
+
 from ase.calculators.singlepoint import SinglePointCalculator
 from ase.units import Bohr
 from ase.io import Trajectory
+from ase.optimize.optimize import Optimizer
+
 
 def rs_newton_irc(pes, sqrtm, g, d1, dx, xi=1.):
     lams = pes.lams
@@ -19,7 +23,6 @@ def rs_newton_irc(pes, sqrtm, g, d1, dx, xi=1.):
     Vg = vecs.T @ g
 
     W = np.diag(sqrtm**2)
-    Wd1 = W @ d1
 
     xilower = 0
     xiupper = None
@@ -37,7 +40,6 @@ def rs_newton_irc(pes, sqrtm, g, d1, dx, xi=1.):
         T0W = np.linalg.solve(Hshift, W)
         eps = -T0g - xi * T0W @ d1
         deps = -T0W @ (d1 + eps)
-        #deps = -(T0 @ W @ (d1 + eps))
 
         dxw = (d1 + eps) * sqrtm
         dxwmag = np.linalg.norm(dxw)
@@ -69,114 +71,107 @@ def rs_newton_irc(pes, sqrtm, g, d1, dx, xi=1.):
     return eps, xi, True
 
 
-def irc(pes, maxiter, ftol, irctol=1e-4, dx=0.2 * Bohr, direction='both', **kwargs):
-    try:
-        from ase.data import atomic_masses_common
-    except ImportError:
-        warnings.warn("The version of ASE that is installed does not contain "
-                      "the most common isotope masses, so Earth-abundance-"
-                      "averaged masses will be used instead!")
-        mass_type = "defaults"
-    else:
-        mass_type = "most_common"
+class IRC(Optimizer):
+    def __init__(self, atoms, restart=None, logfile='-', trajectory=None,
+                 master=None, force_consistent=False, irctol=1e-4, dx=0.1,
+                 eta=1e-4, gamma=0.4, peskwargs=None, **kwargs):
+        if isinstance(atoms, PESWrapper):
+            self.pes = atoms
+            atoms = self.pes.atoms
+        else:
+            self.pes = PESWrapper(atoms, atoms.calc, **kwargs)
+        Optimizer.__init__(self, atoms, restart, logfile, trajectory, master,
+                           force_consistent)
+        self.irctol = irctol
+        self.dx = dx
+        if peskwargs is None:
+            self.peskwargs = dict(eta=eta, gamma=gamma)
 
-    pes.atoms.set_masses(mass_type)
+        if 'masses' not in self.atoms.arrays:
+            try:
+                self.atoms.set_masses('most_common')
+            except ValueError:
+                warnings.warn("The version of ASE that is installed does not "
+                              "contain the most common isotope masses, so "
+                              "Earth-abundance-averaged masses will be used "
+                              "instead!")
+                self.atoms.set_masses('defaults')
 
-    if direction not in ['forward', 'reverse', 'both']:
-        raise ValueError("Don't understand direction='{}'".format(direction))
+        self.sqrtm = np.sqrt(self.atoms.get_masses()[:, np.newaxis]
+                             * np.ones_like(self.atoms.positions)).ravel()
 
-    conf = pes.atoms.copy()
-    calc = SinglePointCalculator(conf, **pes.atoms.calc.results)
-    conf.set_calculator(conf)
-    path = [conf]
-    f1, g1 = pes.evaluate(pes.x.copy())
+        self.lastrun = None
+        self.x0 = self.pes.x.copy()
+        self.v0ts = None
+        self.H0 = None
+        self.peslast = None
+        self.xi = 1.
+        self.first = True
 
-    pes.diag(**kwargs)
+    def irun(self, fmax=0.05, steps=None, direction='forward'):
+        if direction not in ['forward', 'reverse']:
+            raise ValueError('direction must be one of "forward" or '
+                             '"reverse"!')
 
-    if np.linalg.norm(g1) > ftol:
-        warnings.warn('Initial forces are greater than convergence tolerance! '
-                      'Are you sure this is a transition state?')
+        if self.v0ts is None:
+            # Initial diagonalization
+            self.pes.diag(**self.peskwargs)
+            Hw = self.pes.H / np.outer(self.sqrtm, self.sqrtm)
+            _, vecs = eigh(Hw)
+            self.v0ts = self.dx * vecs[:, 0] / self.sqrtm
+            self.H0 = self.pes.H.copy()
+            self.peslast = self.pes.last.copy()
+        else:
+            # Or, restore from last diagonalization for new direction
+            self.pes.x = self.x0.copy()
+            self.pes.H = self.H0.copy()
+            self.pes.last = self.peslast.copy()
 
-    # Square root of the mass array, not to be confused with
-    # scipy.linalg.sqrtm, the matrix square root function.
-    sqrtm = np.sqrt(pes.atoms.get_masses()[:, np.newaxis]
-                    * np.ones_like(pes.atoms.positions)).ravel()
+        if direction == 'forward':
+            self.d1 = self.v0ts.copy()
+        elif direction == 'reverse':
+            self.d1 = -self.v0ts.copy()
 
-    Hw = pes.H / np.outer(sqrtm, sqrtm)
-    _, vecs = eigh(Hw)
-    d1w = dx * vecs[:, 0]
-    d1 = d1w / sqrtm
+        self.first = True
+        return Optimizer.irun(self, fmax, steps)
 
-    if direction == 'forward':
-        return _irc_half(pes, maxiter, ftol, d1, irctol, dx, sqrtm, 'forward')
-    elif direction == 'reverse':
-        return _irc_half(pes, maxiter, ftol, -d1, irctol, dx, sqrtm, 'reverse')
-    elif direction == 'both':
-        x0 = pes.x.copy()
-        last = pes.last.copy()
-        H = pes.H.copy()
-        path = list(reversed(_irc_half(pes, maxiter, ftol, d1, irctol, dx, sqrtm, 'forward')))
-        pes.x = x0
-        pes.last = last
-        pes.H = H
-        path += _irc_half(pes, maxiter, ftol, -d1, irctol, dx, sqrtm, 'reverse')[1:]
-        return path
+    def run(self, fmax=0.05, steps=None, direction='forward'):
+        for converged in self.irun(fmax, steps, direction):
+            pass
+        return converged
 
-def _irc_half(pes, maxiter, ftol, d1, irctol, dx, sqrtm, label='forward'):
-    traj = Trajectory('sella_irc_{}.traj'.format(label), 'w', pes.atoms)
-    f1 = pes.last['f']
-    g1 = pes.last['g']
-    traj.write(pes.atoms, energy=f1, forces=-g1.reshape((-1, 3)))
-    path = []
-    # Outer loop finds all points along the MEP
-    xi = 1.
-    converged = False
-    first = True
-    x0 = pes.x.copy()
-    while True:
-        # Back up current geometry and projection matrix, Tm
-        Tm = pes.Tm.copy()
-        # Inner loop optimizes each individual point along the MEP
+    def step(self):
+        x0 = self.pes.x.copy()
+        Tm = self.pes.Tm.copy()
+        g1 = self.pes.last['g']
         for _ in range(100):
-            #f1last, g1last, d1last = f1, g1.copy(), d1.copy()
-            if first:
-                epsnorm = np.linalg.norm(d1)
+            if self.first:
+                epsnorm = np.linalg.norm(self.d1)
                 bound_clip = True
-                first = False
+                self.first = False
             else:
-                eps, xi, bound_clip = rs_newton_irc(pes, sqrtm, g1, d1, dx, xi)
+                eps, self.xi, bound_clip = rs_newton_irc(self.pes, self.sqrtm,
+                                                         g1, self.d1, self.dx,
+                                                         self.xi)
                 epsnorm = np.linalg.norm(eps)
-                d1 += eps
-                #d1 = (Tm @ Tm.T) @ (d1 + eps)
-            f1, g1 = pes.evaluate(x0 + d1)
-            #if f1 > f1last:
-            #    f1, g1, d1 = f1last, g1last.copy(), d1last.copy()
-            #    continue
+                self.d1 += eps
+            f1, g1 = self.pes.evaluate(x0 + self.d1)
 
-            if np.linalg.norm(g1) < ftol and pes.lams[0] > 0:
-                print('IRC done')
-                converged = True
-                break
-
-            d1m = d1 * sqrtm
-            g1m = ((Tm @ Tm.T) @ g1) / sqrtm
-            dot = np.abs(d1m @ g1m) / (np.linalg.norm(d1m) * np.linalg.norm(g1m))
-            print('Epsnorm is {}, dot product is {}, bound clip?: {}'.format(epsnorm, dot, bound_clip))
-            if bound_clip and abs(1 - dot) < irctol:
+            d1m = self.d1 * self.sqrtm
+            d1m /= np.linalg.norm(d1m)
+            g1m = ((Tm @ Tm.T) @ g1) / self.sqrtm
+            g1m /= np.linalg.norm(g1m)
+            dot = np.abs(d1m @ g1m)
+            print('Epsnorm is {}, dot product is {}, bound clip?: {}'
+                  ''.format(epsnorm, dot, bound_clip))
+            if bound_clip and abs(1 - dot) < self.irctol:
                 print('Found new point on IRC')
                 break
         else:
             raise RuntimeError("Inner IRC loop failed to converge")
-        traj.write(pes.atoms, energy=f1, forces=-g1.reshape((-1, 3)))
+        g1w = g1 / self.sqrtm
+        d1w = -self.dx * g1w / np.linalg.norm(g1w)
+        self.d1 = d1w / self.sqrtm
 
-        conf = pes.atoms.copy()
-        calc = SinglePointCalculator(conf, **pes.atoms.calc.results)
-        conf.set_calculator(calc)
-        path.append(conf)
-        if converged:
-            return path
-
-        x0 += d1
-        g1w = g1 / sqrtm
-        d1w = -dx * g1w / np.linalg.norm(g1w)
-        d1 = d1w / sqrtm
+    def converged(self, forces=None):
+        return Optimizer.converged(self, forces) and self.pes.lams[0] > 0
