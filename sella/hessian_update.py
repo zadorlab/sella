abd2f7847d0a76397f374a33d67ec16627157147
Eric Hermes <ehermes@sandia.gov>
2019-11-13 14:41:04 -0800

    Algorithmic improvement to Hessian initialization

--- sella/hessian_update.py
+++ sella/hessian_update.py
@@ -36,7 +36,8 @@ def update_H(B, S, Y, method='TS-BFGS', symm=2, lams=None, vecs=None):
         # Approximate B as a scaled identity matrix, where the
         # scalar is the average Ritz value from S.T @ Y
         thetas, _ = eigh(S.T @ Ytilde, S.T @ S)
-        lam0 = np.average(np.abs(thetas))
+        #lam0 = np.average(np.abs(thetas))
+        lam0 = np.exp(np.average(np.log(np.abs(thetas))))
         d, _ = S.shape
         B = lam0 * np.eye(d)
 
