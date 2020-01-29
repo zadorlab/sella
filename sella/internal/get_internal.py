fix hessian of constraint and more minor fixes

sella/internal/get_internal.py
@@ -3,6 +3,7 @@ import numpy as np
 from sella.constraints import (get_constraints, merge_internal_constraints,
                                cons_to_dict)
 from .int_find2 import find_angles, find_dihedrals
+from sella.internal.int_eval import get_bond
 from sella.internal.int_find import find_bonds
 from sella.internal.int_classes import CartToInternal
 
@@ -20,16 +21,40 @@ def extract_dummy_constraints(natoms, con_user):
         con_dummy[key] = np.array(data, dtype=np.int32).reshape((-1, size))
     return con_dummy
 
+def check_bad_bonds(atoms, bonds):
+    if bonds is None:
+        out = np.empty((0, 2), dtype=np.int32)
+        return out, out
+    added_bonds = []
+    forbidden_bonds = []
+    g = -atoms.get_forces().ravel()
+    for i, j in bonds:
+        q, dqdx = get_bond(atoms, i, j, grad=True)
+        if (dqdx.ravel() @ g) > 0:
+            added_bonds.append([i, j])
+        else:
+            forbidden_bonds.append([i, j])
+    added_bonds = np.array(added_bonds, dtype=np.int32)
+    if added_bonds.size == 0:
+        added_bonds = np.empty((0, 2), dtype=np.int32)
+    forbidden_bonds = np.array(forbidden_bonds, dtype=np.int32)
+    if forbidden_bonds.size == 0:
+        forbidden_bonds = np.empty((0, 2), dtype=np.int32)
+    return added_bonds, forbidden_bonds
+
 
 def get_internal(atoms, con_user=None, target_user=None, atol=15.,
-                 dummies=None, conslast=None):
+                 dummies=None, conslast=None, check_bonds=None,
+                 check_angles=None):
     if con_user is None:
         con_user = dict()
 
     if target_user is None:
         target_user = dict()
 
-    bonds, nbonds, c10y = find_bonds(atoms)
+    added_bonds, forbidden_bonds = check_bad_bonds(atoms, check_bonds)
+
+    bonds, nbonds, c10y = find_bonds(atoms, added_bonds, forbidden_bonds)
 
     if conslast is None:
         dinds = None
@@ -42,7 +67,8 @@ def get_internal(atoms, con_user=None, target_user=None, atol=15.,
     # FIXME: This looks hideous, can it be improved?
     (angles, dummies, dinds, angle_sums,
      bcons, acons, dcons, adiffs) = find_angles(atoms, atol, bonds, nbonds,
-                                                c10y, dummies, dinds)
+                                                c10y, dummies, dinds,
+                                                check_angles)
 
     dihedrals = find_dihedrals(atoms + dummies, atol, bonds, angles, nbonds,
                                c10y, dinds)

sella/internal/int_find2.py
@@ -20,7 +20,7 @@ def get_dummy(atoms, nbonds, c10y, i, j, k, atol):
                 dx1 -= dx2 * (dx1 @ dx2)
                 dx1 /= np.linalg.norm(dx1)
                 dpos = dx1 + atoms.positions[j]
-                return dpos, n, m
+                return dpos, n, m, True
 
     dx1 = atoms.get_distance(j, i, vector=True)
     dx2 = atoms.get_distance(j, k, vector=True)
@@ -34,18 +34,26 @@ def get_dummy(atoms, nbonds, c10y, i, j, k, atol):
         dpos /= np.linalg.norm(dpos)
     else:
         dpos /= dpos_norm
+    dpos += atoms.positions[j]
 
-    return dpos, i, k
+    return dpos, i, k, False
 
 
 def check_if_planar(dxs, atol):
     dxs /= np.linalg.norm(dxs, axis=1)[:, np.newaxis]
     U, s, VT = np.linalg.svd(dxs)
-    angles = np.arccos(dxs @ VT[:, 2])
-    return np.max(angles) - np.min(angles) > atol
+    angles = np.arccos(dxs @ VT[:, 2]) * 180 / np.pi
+    return np.max(angles) - np.min(angles) < atol
 
+def check_angle(atoms, i, j, k, atol, bad_angles):
+    if bad_angles is not None:
+        for a, b, c in bad_angles:
+            if b == j and ((a == i and c == k) or (a == k and c == i)):
+                return False
+    return atol < atoms.get_angle(i, j, k) < 180 - atol
 
-def find_angles(atoms, atol, bonds, nbonds, c10y, dummies=None, dinds=None):
+def find_angles(atoms, atol, bonds, nbonds, c10y, dummies=None, dinds=None,
+                bad_angles=None):
     atoms = atoms.copy()
     natoms = len(atoms)
 
@@ -71,23 +79,26 @@ def find_angles(atoms, atol, bonds, nbonds, c10y, dummies=None, dinds=None):
             raise RuntimeError("Atom {} has no bonds!".format(j))
         elif nj == 2:
             i, k = c10y[j, :2]
-            if atol < atoms.get_angle(i, j, k) < 180 - atol:
+            if check_angle(atoms, i, j, k, atol, bad_angles):
                 angles.append([i, j, k])
             elif dinds[j] < 0:
                 dinds[j] = natoms + ndummies
-                dpos, a, b = get_dummy(atoms, nbonds, c10y, i, j, k, atol)
+                dpos, a, b, proper = get_dummy(atoms, nbonds, c10y, i, j, k, atol)
                 dummies += Atom('X', dpos)
 
                 bond_constraints.append([j, dinds[j]])
                 angle_constraints.append([a, j, dinds[j]])
-                dihedral_constraints.append([b, a, j, dinds[j]])
+                if proper:
+                    dihedral_constraints.append([b, a, j, dinds[j]])
+                else:
+                    dihedral_constraints.append([b, j, dinds[j], a])
 
                 ndummies += 1
         else:
             linear = []
             for a, i in enumerate(c10y[j, :nj-1]):
                 for k in c10y[j, a+1:nj]:
-                    if atol < atoms.get_angle(i, j, k) < 180 - atol:
+                    if check_angle(atoms, i, j, k, atol, bad_angles):
                         angles.append([i, j, k])
                     else:
                         linear.append(sorted([i, k], key=lambda x: atoms.get_distance(x, j)))

sella/peswrapper.py
@@ -59,6 +59,7 @@ class PES:
         self.set_H(H0)
 
         self.savepoint = dict(apos=None, dpos=None)
+        self.first_diag = True
 
     apos = property(lambda self: self.atoms.positions.copy())
     dpos = property(lambda self: None)
@@ -206,13 +207,16 @@ class PES:
         Unred = self.get_Unred()
 
         P = self.get_HL().project(Unred)
-        v0 = None
 
-        if P.B is None:
-            P = np.eye(len(self.get_x()))
+        if P.B is None or self.first_diag:
             v0 = self.v0
             if v0 is None:
                 v0 = self.get_g() @ Unred
+        else:
+            v0 = None
+
+        if P.B is None:
+            P = np.eye(len(self.get_x()))
         else:
             P = P.asarray()
 
@@ -237,6 +241,8 @@ class PES:
 
         # Update the approximate Hessian
         self.H.update(Vs, AVs)
+
+        self.first_diag = False
 
     # FIXME: temporary functions for backwards compatibility
     def get_projected_forces(self):
@@ -300,7 +306,7 @@ class InternalPES(PES):
         self.set_H(H0)
 
         # Flag used to indicate that new internal coordinates are required
-        self.bad_int = False
+        self.bad_int = None
 
     dpos = property(lambda self: self.dummies.positions.copy())
 
@@ -317,9 +323,10 @@ class InternalPES(PES):
             ode.step()
             y = ode.y
             t0 = ode.t
-            if self.int.check_for_bad_internal(self.apos, self.dpos):
+            self.bad_int = self.int.check_for_bad_internal(self.apos,
+                                                           self.dpos)
+            if self.bad_int is not None:
                 print('Bad internals found!')
-                self.bad_int = True
                 break
             if ode.nfev > 1000:
                 view(self.atoms + self.dummies)
@@ -341,7 +348,11 @@ class InternalPES(PES):
     def get_Hc(self):
         D_cons = self.cons.get_D(self.apos, self.dpos)
         Binv_int = self.int.get_Binv(self.apos, self.dpos)
-        Hc = Binv_int.T @ D_cons.ldot(self.curr['L']) @ Binv_int
+        B_cons = self.cons.get_B(self.apos, self.dpos)
+        D_int = self.int.get_D(self.apos, self.dpos)
+        L_int = self.curr['L'] @ B_cons @ Binv_int
+        Hc = Binv_int.T @ (D_cons.ldot(self.curr['L'])
+                           - D_int.ldot(L_int)) @ Binv_int
         return Hc
 
     def get_drdx(self):
@@ -351,9 +362,11 @@ class InternalPES(PES):
     def _calc_basis(self):
         drdx = self.get_drdx()
         Ucons = modified_gram_schmidt(drdx.T)
+        na = 3 * len(self.atoms)
         B = self.int.get_B(self.apos, self.dpos)
+        Udummy = modified_gram_schmidt(B[:, na:])
         Binv = self.int.get_Binv(self.apos, self.dpos)
-        Unred = modified_gram_schmidt(B @ Binv)
+        Unred = modified_gram_schmidt(B @ Binv, Udummy)
         Ufree = modified_gram_schmidt(Unred, Ucons)
         return drdx, Ucons, Unred, Ufree
 
@@ -364,9 +377,18 @@ class InternalPES(PES):
 
     def update_internals(self, dx):
         self._update(True)
+
+        if self.bad_int is None:
+            check_bonds = None
+            check_angles = None
+        else:
+            check_bonds = self.bad_int['bonds']
+            check_angles = self.bad_int['angles']
+
         # Find new internals, constraints, and dummies
         out = get_internal(self.atoms, self.con_user, self.target_user,
-                           dummies=self.dummies, conslast=self.cons)
+                           dummies=self.dummies, conslast=self.cons,
+                           check_bonds=check_bonds, check_angles=check_angles)
         new_int, new_cons, new_dummies = out
         nold = 3 * (len(self.atoms) + len(self.dummies))
 
@@ -394,7 +416,6 @@ class InternalPES(PES):
         H = self.get_H().asarray()
         Hcart = Blast.T @ H @ Blast + Dlast.ldot(self.curr['g'])
         Hnew = Binv.T[:, :nold]@Hcart@Binv[:nold] - Binv.T@D.ldot(g) @ Binv
-        Hnew += P2.T @ new_int.guess_hessian(self.atoms, new_dummies) @ P2.T
         self.dim = len(x)
         self.set_H(Hnew)
 
@@ -446,20 +467,17 @@ class InternalPES(PES):
     def kick(self, dx, diag=False, **diag_kwargs):
         ratio = PES.kick(self, dx, diag=diag, **diag_kwargs)
 
-        if self.bad_int:
+        if self.bad_int is not None:
             self.update_internals(dx)
-            self.bad_int = False
+            self.bad_int = None
 
         return ratio
 
     def _update_H(self):
         if self.last['x'] is None or self.last['g'] is None:
             return
-        Unred = self.get_Unred()
-        P = Unred @ Unred.T
-        dx = P @ self.wrap_dx(self.curr['x'] - self.last['x'])
-        dg = P @ (self.curr['g'] - self.last['g'])
-        self.H = self.H.project(P)
+        dx = self.wrap_dx(self.curr['x'] - self.last['x'])
+        dg = self.curr['g'] - self.last['g']
         self.H.update(dx, dg)
 
     def write_traj(self):