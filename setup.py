58fab7accf4933872c292c5816c8bb96a658d68b
Eric Hermes <ehermes@sandia.gov>
2021-05-10 12:14:36 -0700

    update version number for zenodo

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open('requirements.txt', 'r') as f:
     install_requires = f.read().strip().split()
 
 setup(name='Sella',
-      version='2.0.2',
+      version='2.0.2.geo1',
       author='Eric Hermes',
       author_email='ehermes@sandia.gov',
       long_description=long_description,
