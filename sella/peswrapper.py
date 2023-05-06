Hessian conversion/diag_every_n fix
Conversion between Cartesian and internal Hessians were being performed
incorrectly; this has been fixed. I have verified that setting the null
space of the Hessian to be the geometric mean of the absolute
eigenvalues of the non-redundant space improves performance when
diag_every_n > 0, as expected.

Additionally, reset the steps since diagonalization when initializing
Hessian matrix.

sella/peswrapper.py
@@ -649,7 +649,7 @@ class InternalPES(PES):
         Ured = Ui[:, nnred:]
 
         # Calculate inverse Jacobian in non-redundant space
-        Bnred_inv = VTi[:nnred].T @ np.diag(1 / Si[:nnred]) @ Unred.T
+        Bnred_inv = VTi[:nnred].T @ np.diag(1 / Si[:nnred])
 
         # Convert Cartesian Hessian to non-redundant internal Hessian
         Hcart_corr = Hcart - self.int.hessian().ldot(self.get_g())
@@ -663,8 +663,7 @@ class InternalPES(PES):
         lnred_mean = np.exp(np.log(np.abs(lnred)).mean())
 
         # finish reconstructing redundant internal Hessian
-        P = Ui.T @ Unred
-        return P @ Hnred @ P.T + lnred_mean * Ured @ Ured.T
+        return Unred @ Hnred @ Unred.T + lnred_mean * Ured @ Ured.T
 
     def _convert_internal_hessian_to_cartesian(
         self,