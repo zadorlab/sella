56b974e803fbe130dd2cb5377584dea4a81910cb
Eric Hermes <ehermes@sandia.gov>
2023-06-15 17:22:21 -0400

    add trajectory field to Sella object

--- sella/optimize/optimize.py
+++ sella/optimize/optimize.py
@@ -205,6 +205,7 @@ class Sella(Optimizer):
                 hessian_function=hessian_function,
                 **kwargs
             )
+        self.trajectory = self.pes.traj
 
     def _predict_step(self):
         if not self.initialized:
