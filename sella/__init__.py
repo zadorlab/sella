7a7e426c4cc25077ee6e0a541a15be944d61c0fd
Andrew S. Rosen <asrosen93@gmail.com>
2024-02-26 14:29:39 -0800

    Support `jax>=0.4.25`
    Closes #35.
    
    As noted in the [jax changelog](https://jax.readthedocs.io/en/latest/changelog.html#jax-0-4-25-feb-26-2024), the `jax.config` API has been removed. You are now supposed to `import jax` and then do `jax.config` to modify the config. That's what I've done in this PR.

--- sella/__init__.py
+++ sella/__init__.py
@@ -1,7 +1,8 @@
+import jax
+
 from .optimize import IRC, Sella
 from .internal import Internals, Constraints
-from jax.config import config
 
-config.update("jax_enable_x64", True)
+jax.config.update("jax_enable_x64", True)
 
 __all__ = ['IRC', 'Sella']
